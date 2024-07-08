88c1e75608567762213d7689e33d23270c71ff40
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-07-08 23:12:22 +0530

    Adding Global Exception Handling class for Student RestController.

--- src/main/java/com/RestServices/RestServices/Controllers/StudentController.java
+++ src/main/java/com/RestServices/RestServices/Controllers/StudentController.java
@@ -1,7 +1,12 @@
 package com.RestServices.RestServices.Controllers;
 
+import com.RestServices.RestServices.ExceptionHandling.StudentErrorResponse;
+import com.RestServices.RestServices.ExceptionHandling.StudentNotFoundException;
 import com.RestServices.RestServices.Pojos.Student;
 import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +31,12 @@ public class StudentController {
     }
 
     @GetMapping("/students/{studentID}")
-    public Student getStudentById(@PathVariable int studentID){
+    public Student getStudentById(@PathVariable int studentID) throws StudentNotFoundException {
+        if(studentID>=studentList.size()||studentID<0){
+            throw new StudentNotFoundException("Student not Found! - "+studentID);
+        }
         return studentList.get(studentID);
     }
+
+
 }
